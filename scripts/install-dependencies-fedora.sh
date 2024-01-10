343023cffccd31589ef6da760432826cc1fba317
Matheus Alpes <matheusalpes@proton.me>
2024-01-10 13:45:12 -0300

    Add missing dependencies to build on Fedora/Nobara 39

--- scripts/install-dependencies-fedora.sh
+++ scripts/install-dependencies-fedora.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dnf install cmake extra-cmake-modules gcc-c++ qt5-qtbase-devel qt5-qtdeclarative-devel qt5-qtx11extras-devel kf5-plasma-devel kf5-kglobalaccel-devel kf5-kxmlgui-devel
+dnf install cmake extra-cmake-modules gcc-c++ qt5-qtbase-devel qt5-qtdeclarative-devel qt5-qtx11extras-devel kf5-plasma-devel kf5-kglobalaccel-devel kf5-kxmlgui-devel kf5-wayland kf5-wayland-devel qt5-qtwayland-devel plasma-workspace-wayland plasma-workspace-devel kf5-kitemmodels kf5-kitemmodels-devel
